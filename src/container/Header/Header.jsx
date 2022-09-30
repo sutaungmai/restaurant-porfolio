3c5dfc806ea72efa9ac9f3fc5800db8afceef07b
sutaungmai <patrickgumrai@gmail.com>
2022-09-30 14:25:04 +0700

    header and aboutus

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { SubHeading } from '../../components';
 
+import {images} from '../../constants'
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className='app__wrapper_info'>
+      <SubHeading title="Chase the new Flavour" />
+      <h1 className='app__header_h1'>The Key to Fine Dining</h1>
+      <p className='p__opensans' style={{margin: '2rem 0'}}> Sit Tellus Lobortis Sed Senectus Vivamus Molestie. Condimentum Volutpat Morbi Facilisis Quam Scelerisque Sapien. Et, Penatibus Aliquam Amet Tellus. </p>
+      <button type='button' className='custom__button'>Explore Menu</button>
+    </div>
+
+    <div className='app__wrapper_img'>
+      <img src={images.welcome} alt="header img" />
+    </div>
   </div>
 );
 
